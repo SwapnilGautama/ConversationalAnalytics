78fe29f5b9611c1657d1816efa73eb64c7ef1d03
SwapnilGautama <swapnil.gautama@gmail.com>
2025-07-31 10:31:51 +0530

    Update headcount_aggregated.py

--- kpi_engine/headcount_aggregated.py
+++ kpi_engine/headcount_aggregated.py
@@ -5,7 +5,7 @@ def get_headcount_aggregated(ut_path):
 
     df.columns = df.columns.str.strip()
     df['Date_a'] = pd.to_datetime(df['Date_a'], errors='coerce')
-    df['Month'] = df['date_a'].dt.month.map({
+    df['Month'] = df['Date_a'].dt.month.map({
         1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun',
         7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'
     })
